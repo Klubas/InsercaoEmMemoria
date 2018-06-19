96e2a34d938e68c53a7c0dc7a90081d70ede1e50
Lucas Kluber <lucas.kluber@gmail.com>
2018-06-19 18:37:02 -0300

    diretorio includes

--- lista.cpp
+++ lista.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lista.h"
+#include "includes/lista.h"
 
 int insereLista(int valor, No *lista){
 	No *atual;
--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include "memoria.h"
-#include "lista.h"
+#include "includes/memoria.h"
 
-No memff, membf;
+Mem mem;
 
 int menu(){
 	/*criar_mem(mem0, 1000);
@@ -15,36 +14,15 @@ int menu(){
 }
 
 int main(){
-	int tam = 10;
-/*	Mem memoria;
-	memoria.tam = tam;
-	memoria.ff = NULL;
-	memoria.bf = NULL;
-	memoria.wf = NULL;
-	memoria.nf = NULL;
-	Mem *mptr = &memoria;
-*/
 
-	criar_mem(10, &memff);
-	criar_mem(20, &membf);
-	listar_mem(&memff);
-    listar_mem(&membf);
+	criar_mem(10, &mem.ff);
+	criar_mem(20, &mem.bf);
+	listar_mem(&mem.ff);
+    listar_mem(&mem.bf);
 
-
-	//printf("[%d]", mem0.dado);
-
-	//criar_mem(15, mem0ptr);
-
-	//criar_mem(9, memoria.bf);
-	//criar_mem(8, memoria.nf);
-	//criar_mem(7, memoria.wf);
-	//imprime(memoria.wf);
 
 	/*while(menu() > 0);*/
-	//	printf("teste0");
-	//criar_mem(100);
-	//printf("teste1");
-	//imprime(mem0);
+
 
 	return 0;	
 }
